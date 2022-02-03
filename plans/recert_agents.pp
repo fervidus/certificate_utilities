5b643e258e2ff92ec569a2e91061c535c6f6b1c7
Bryan Belanger <bryan@fervid.us>
2022-02-03 15:55:26 -0500

    X

--- plans/recert_agents.pp
+++ plans/recert_agents.pp
@@ -20,14 +20,15 @@ plan certificate_utilities::recert_agents (
   # Go through each agent one at a time
   $expiring_agents.each | String $agent_file | {
     if $agent_file =~ /([^\/]+).pem/ {
-      $next_command = "HOME=/root && export HOME && puppet infrastructure run regenerate_agent_certificate agent=${1}"
+      # $next_command = "HOME=/root && export HOME && puppet infrastructure run regenerate_agent_certificate agent=${1}"
+      $next_command = run_task('certificate_utilities::run_agent_recert', $ca_target, 'agent' => $1)
 
-      $recert_results = run_command($next_command, $ca_target, '_catch_errors' => true)
+      # $recert_results = run_command($next_command, $ca_target, '_catch_errors' => true)
 
-      # Failure print notify
-      if($recert_results.first.status == 'failure') {
-        out::message("Agent ${1} failed")
-      }
+      # # Failure print notify
+      # if($recert_results.first.status == 'failure') {
+      #   out::message("Agent ${1} failed")
+      # }
     }
   }
 }
---
+++ tasks/run_agent_recert.rb
@@ -0,0 +1,22 @@
+#!/opt/puppetlabs/puppet/bin/ruby
+# frozen_string_literal: true
+
+require 'uri'
+require 'net/http'
+require 'json'
+require 'openssl'
+
+require_relative '../../ruby_task_helper/files/task_helper'
+
+# documentation comment
+class RunAgentRecert < TaskHelper
+  def task(name: nil, **kwargs)
+    cmd = "HOME=/root && export HOME && puppet infrastructure run regenerate_agent_certificate agent=#{kwargs['agent']}"
+    stdout, stderr, status = Open3.capture3(cmd)
+
+    raise Puppet::Error, _("stderr: '#{stderr}'") if status != 0
+    { status: stdout.strip }
+  end
+end
+
+RunAgentRecert.run if __FILE__ == $PROGRAM_NAME
