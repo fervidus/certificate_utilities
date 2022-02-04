729088c1705ddf03dd60d212bf4df10ee5905f3f
Bryan Belanger <bryan@fervid.us>
2022-02-04 09:16:52 -0500

    Run agent cert tasks.

--- tasks/run_agent_recert.rb
+++ tasks/run_agent_recert.rb
@@ -9,11 +9,21 @@ require_relative '../../ruby_task_helper/files/task_helper'
 # documentation comment
 class RunAgentRecert < TaskHelper
   def task(name: nil, **kwargs)
-    cmd = "HOME=/root && export HOME && puppet infrastructure run regenerate_agent_certificate agent=#{kwargs[:agent]}"
-    stdout, stderr, status = Open3.capture3(cmd)
+    if(kwargs[:date]) {
+      cmd = "puppet task run ca_extend::check_agent_expiry"
+    }
+    else {
+      cmd = "puppet task run ca_extend::check_agent_expiry date=#{kwargs[:date]}"
+    }
 
-    raise Puppet::Error, _("stderr: '#{stderr}'") if status != 0
+    stdout, stderr, status = Open3.capture3(cmd)
     stdout.strip.to_json
+
+    # cmd = "HOME=/root && export HOME && puppet infrastructure run regenerate_agent_certificate agent=#{kwargs[:agent]}"
+    # stdout, stderr, status = Open3.capture3(cmd)
+
+    # raise Puppet::Error, _("stderr: '#{stderr}'") if status != 0
+    # stdout.strip.to_json
   end
 end
 
