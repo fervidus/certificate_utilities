3cb92c905f858df2bf6ee41f2b119f961d7f6055
Bryan Belanger <bryan@fervid.us>
2022-02-04 14:25:00 -0500

    New plan

--- plans/recert_agents.pp
+++ plans/recert_agents.pp
@@ -17,21 +17,25 @@ plan certificate_utilities::recert_agents (
 
   $expiring_agents = $expiring_agents_results.first['expiring']
 
-  $failed_agents = []
+  out::message($expiring_agents)
+
+  # $failed_agents = []
 
   # Go through each agent one at a time
-  $expiring_agents.each | String $agent_file | {
-    if $agent_file =~ /([^\/]+).pem/ {
+  # $expiring_agents.each | String $agent_file | {
+  #   if $agent_file =~ /([^\/]+).pem/ {
       # $next_command = "HOME=/root && export HOME && puppet infrastructure run regenerate_agent_certificate agent=${1}"
-      $next_command = run_task('certificate_utilities::run_agent_recert', $ca_target, 'agent' => $1, '_catch_errors' => true)
+      # $next_command = run_task('certificate_utilities::run_agent_recert', $ca_target, 'agent' => $1, '_catch_errors' => true)
+      $next_command = run_plan('peadm::modify_certificate', 'tester.azcender.com', 'primary_host' => 'puppet.azcender.com', 'force_regenerate' => true)
 
       # Failure print notify
-      if($recert_results.first.status == 'failure') {
-        $failed_agents << "Agent ${1} failed"
-      }
-    }
-  }
+      # if($recert_results.first.status == 'failure') {
+      #   $failed_agents << "Agent ${1} failed"
+      # }
+  #   }
+  # }
 
   # Return failures
-  return $failed_agents
+  # return $failed_agents
+  out::message($next_command.first.message)
 }
