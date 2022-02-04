0fc59774fd80a28c5827d31efb40858e288d7b47
Bryan Belanger <bryan@fervid.us>
2022-02-04 13:37:54 -0500

    X

--- tasks/run_agent_recert.rb
+++ tasks/run_agent_recert.rb
@@ -9,13 +9,13 @@ require_relative '../../ruby_task_helper/files/task_helper'
 # documentation comment
 class RunAgentRecert < TaskHelper
   def task(name: nil, **kwargs)
-    cmd = ''
+    cmd = 'puppet access show'
 
-    if(kwargs[:date])
-      cmd = "puppet task run ca_extend::check_agent_expiry -n puppet.azcender.com"
-    else
-      cmd = "puppet task run ca_extend::check_agent_expiry date=#{kwargs[:date]} -n puppet.azcender.com"
-    end
+    # if(kwargs[:date])
+    #   cmd = "puppet task run ca_extend::check_agent_expiry -n puppet.azcender.com"
+    # else
+    #   cmd = "puppet task run ca_extend::check_agent_expiry date=#{kwargs[:date]} -n puppet.azcender.com"
+    # end
 
     stdout, stderr, status = Open3.capture3(cmd)
     raise Puppet::Error, _("stderr: '#{stderr}'") if status != 0
