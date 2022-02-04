6a896919ba1661d1c4cb50c2f1647d703b1ecf48
Bryan Belanger <bryan@fervid.us>
2022-02-04 13:50:48 -0500

    X

--- tasks/run_agent_recert.rb
+++ tasks/run_agent_recert.rb
@@ -9,17 +9,26 @@ require_relative '../../ruby_task_helper/files/task_helper'
 # documentation comment
 class RunAgentRecert < TaskHelper
   def task(name: nil, **kwargs)
-    cmd = 'puppet access show'
+    output = {}
 
-    # if(kwargs[:date])
-    #   cmd = "puppet task run ca_extend::check_agent_expiry -n puppet.azcender.com"
-    # else
-    #   cmd = "puppet task run ca_extend::check_agent_expiry date=#{kwargs[:date]} -n puppet.azcender.com"
-    # end
+    token_cmd = 'puppet access show'
+
+    stdout, stderr, status = Open3.capture3(token_cmd)
+    raise Puppet::Error, _("stderr: '#{stderr}'") if status != 0
+    output['token'] = stdout.strip
+
+    if(kwargs[:date])
+      cmd = "puppet task run ca_extend::check_agent_expiry -n puppet.azcender.com"
+    else
+      cmd = "puppet task run ca_extend::check_agent_expiry date=#{kwargs[:date]} -n puppet.azcender.com"
+    end
 
     stdout, stderr, status = Open3.capture3(cmd)
     raise Puppet::Error, _("stderr: '#{stderr}'") if status != 0
-    stdout.strip.to_json
+
+    output['response'] = stdout.strip
+
+    output.to_json
 
     # cmd = "HOME=/root && export HOME && puppet infrastructure run regenerate_agent_certificate agent=#{kwargs[:agent]}"
     # stdout, stderr, status = Open3.capture3(cmd)
