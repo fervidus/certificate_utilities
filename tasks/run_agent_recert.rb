98f4b354960092c8c585ca96ea18052123cfb547
Bryan Belanger <bryan@fervid.us>
2022-02-04 14:04:42 -0500

    X

--- tasks/run_agent_recert.rb
+++ tasks/run_agent_recert.rb
@@ -11,19 +11,17 @@ class RunAgentRecert < TaskHelper
   def task(name: nil, **kwargs)
     output = {}
 
-    # token_cmd = 'puppet access show'
+    token_cmd = 'puppet access show'
 
-    # stdout, stderr, status = Open3.capture3(token_cmd)
-    # raise Puppet::Error, _("stderr: '#{stderr}'") if status != 0
-    # output['token'] = stdout.strip
+    stdout, stderr, status = Open3.capture3(token_cmd)
+    raise Puppet::Error, _("stderr: '#{stderr}'") if status != 0
+    output['token'] = stdout.strip
 
-    cmd = 'puppet access --help'
-
-    # if(kwargs[:date])
-    #   cmd = "puppet access show && puppet task run ca_extend::check_agent_expiry -n puppet.azcender.com"
-    # else
-    #   cmd = "puppet access show && puppet task run ca_extend::check_agent_expiry date=#{kwargs[:date]} -n puppet.azcender.com"
-    # end
+    if(kwargs[:date])
+      cmd = "puppet task run ca_extend::check_agent_expiry -n puppet.azcender.com"
+    else
+      cmd = "puppet task run ca_extend::check_agent_expiry date=#{kwargs[:date]} -n puppet.azcender.com"
+    end
 
     stdout, stderr, status = Open3.capture3(cmd)
     # raise Puppet::Error, _("stderr: '#{stderr}'") if status != 0
