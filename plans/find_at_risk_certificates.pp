36ba51eb8442c2e27564df79f970aa64d37d8324
Bryan Belanger <bryan@fervid.us>
2022-02-09 15:30:05 -0500

    X

---
+++ plans/find_at_risk_certificates.pp
@@ -0,0 +1,23 @@
+plan certificate_utilities::find_at_risk_certificates (
+  Optional[Integer] $max_days_to_expiration = 90,
+) {
+  # Obtain cas and grab the first one for query
+  $cert_results = puppetdb_query('resources[certname] { }')
+
+  $agent_certs = $cert_results.map | Integer $index, Hash $values | { $values['certname'] }
+
+  $agent_targets = get_targets($agent_certs)
+
+  $expiring_agents_results = run_task('certificate_utilities::find_at_risk_certificates', $agent_targets,
+    'max_days_to_expiration' => $max_days_to_expiration
+  )
+
+  # Go through each agent one at a time
+  $recert_list = $expiring_agents_results.map | String $agent | {
+    if $agent =~ /^.+\.com$/ {
+      $agent
+    }
+  }
+
+  return $recert_list
+}
