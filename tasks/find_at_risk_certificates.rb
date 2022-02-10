2ffe3592da0267a2e2f8d327696c67a70195cc2d
Bryan Belanger <bryan@fervid.us>
2022-02-10 09:04:35 -0500

    X

--- tasks/find_at_risk_certificates.rb
+++ tasks/find_at_risk_certificates.rb
@@ -12,27 +12,22 @@ require_relative '../../ruby_task_helper/files/task_helper'
 # documentation comment
 class GetCertificate < TaskHelper
   def task(name: nil, **kwargs)
-    # cmd = '/opt/puppetlabs/puppet/bin/puppet config print certname'
+    cert_name = `/opt/puppetlabs/puppet/bin/puppet config print certname`
 
-    # stdout, stderr, status = Open3.capture3(*cmd)
+    # Remove problematic whitespace
+    cert_name.strip!
 
-    # raise Puppet::Error, _("stderr: #{stderr}'") if status != 0
+    cert_file = File.new("/etc/puppetlabs/puppet/ssl/certs/#{cert_name}.pem")
 
-    # cert_name = stdout.strip
+    cert = OpenSSL::X509::Certificate.new(cert_file)
 
-    # cert_file = File.new("/etc/puppetlabs/puppet/ssl/certs/#{cert_name}.pem")
+    expire = DateTime.parse(cert.not_after.to_s)
 
-    # cert = OpenSSL::X509::Certificate.new(cert_file)
+    days_from_expiration = expire - kwargs[:max_days_to_expiration]
 
-    # expire = DateTime.parse(cert.not_after.to_s)
-
-    # days_from_expiration = expire - kwargs[:max_days_to_expiration]
-
-    # now = DateTime.now
+    now = DateTime.now
     
-    # puts cert_name if days_from_expiration < now
-
-    puts "Name #{name}\tKwargs: #{kwargs}"
+    puts cert_name if days_from_expiration < now
   end
 end
 
